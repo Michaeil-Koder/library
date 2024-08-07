22f3fb71be96ab904bfdff157fd433348cc5b645
MichaeilZare <147085251+Michaeil-Koder@users.noreply.github.com>
2024-08-07 16:13:39 +0330

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,21 +1,27 @@
-const multer=require("multer")
-const path=require("path")
-
-
-const storage=multer.diskStorage({
-    destination:function(req,file,cb){
-        cb(null,"uploads/")
+const multer = require("multer")
+const path = require("path")
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, "..", "uploads", "covers"))
     },
-    filename:function(req,file,cb){
-        const ext=path.extname(file.originalname)
-        const uniquName=`${path.basename(file.originalname,ext)}_${Math.floor(Math.random()*1E9+Date.now())}`
-        cb(null,uniquName+ext)
-
-    },
-})
-const uploader=multer({
-    storage:storage,
-    limits:{fileSize:3000}
+    filename: function (req, file, cb) {
+        const filename = String(Math.floor(Date.now() + Math.random() * 9999999))
+        const ext = path.extname(file.originalname)
+        const basename = path.basename(file.originalname, ext)
+        cb(null, basename + "_" + filename + ext)
+    }
 })
 
-module.exports=uploader
+const fileFilter = (req, file, cb) => {
+    const imageType = ["image/jpg", "image/jpeg", "image/png", "image/webp"]
+    if (!imageType.includes(file.mimetype)) {
+        cb(new Error("please select image"))
+    } else {
+        cb(null, true)
+    }
+}
+
+module.exports = {
+    storage,
+    fileFilter
+}
